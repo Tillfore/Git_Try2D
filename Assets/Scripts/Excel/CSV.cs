385610e95d8b9faf6171652b8477c0c5b0859c05
Tillfore <tillfore@foxmail.com>
2017-04-16 02:59:20 +0800

    Changes to be committed:

---
+++ Assets/Scripts/Excel/CSV.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+
+
+public class CSV  {
+    static CSV cvs;
+    public List<string[]> m_ArrayData;
+    public static CSV GetInstance()
+    {
+        if (cvs == null) {
+            cvs = new CSV();
+        }
+        return cvs;
+    }
+    private CSV()
+    {
+        m_ArrayData = new List<string[]>();
+    }
+
+    public void LoadFile(string path,string fileName) //读取CVS文件
+    {
+        m_ArrayData.Clear();
+        StreamReader sr = null;
+        try {
+            sr = File.OpenText(path + "//" + fileName);
+        }
+        catch {
+            Debug.Log("Can't find file!");
+            return;
+        }
+        string line;
+        while ((line = sr.ReadLine()) != null) {        //读取行
+            m_ArrayData.Add(line.Split(','));
+        }
+        sr.Close();
+        sr.Dispose();
+    }
+
+    public string GetString(int row,int col)
+    {
+        return m_ArrayData[row][col];
+    }
+    public float GetFloat(int row,int col)
+    {
+        return float.Parse(m_ArrayData[row][col]);
+    }
+
+}
---
+++ Assets/Scripts/Player/Data/EnemyCharacterData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable] //序列化
+public class EnemyCharacterData {
+    //该类在初始化读取后不再调用，以替代数据库或表格
+    [Tooltip("PlayerCharacter")]
+    public int enemyLevel = 0;
+
+}
